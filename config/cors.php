d16925a5a669781f15995ccc3e6a4111aeb8e999
triard <triard78@gmail.com>
2025-03-02 12:06:34 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/steam'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.mahawangsa.com', 'mahawangsa.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
